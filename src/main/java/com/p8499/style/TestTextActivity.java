5c69bf57b32f149a394ca7298cd34cef6bf6c2df
p8499 <p8499@126.com>
2017-02-17 11:10:48 +0800

    20170217

---
+++ src/main/java/com/p8499/style/TestTextActivity.java
@@ -0,0 +1,13 @@
+package com.p8499.style;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_text);
+    }
+}
